a3a26772e46bd2dccef8af023ce61ec8de9ac122
Eongyu Park <peg5325@gmail.com>
2024-08-20 23:50:53 +0900

    스터디 2일차
    백준 1158번 요세푸스 문제
    결과 : 실패

---
+++ week_1/bj_1158_요세푸스.py
@@ -0,0 +1,46 @@
+from sys import stdin
+
+
+input = stdin.readline
+
+n, k = map(int, input().split())
+
+arr = [i for i in range(1, n+1)]
+result_arr = []
+count = 1
+index = 0
+
+while len(arr) != 0:
+    if len(arr) == 1:
+        result_arr.append(arr[0])
+        break
+
+    if count == 3:
+        result_arr.append(arr[index])
+        del arr[index]
+        index -= 1
+        count = 0
+
+    if index == len(arr) - 1:
+        index = 0
+        count += 1
+    else:
+        index += 1
+        count += 1
+
+print("<", end="")
+print(*result_arr, sep=', ', end="")
+print(">")
+
+n, k = map(int, input().split())
+ans = []
+arr = [i for i in range(1, n+1)]
+num = 0
+for i in range(n):
+    num += (k-1)
+    if num >= len(arr):
+        num %= len(arr)
+    ans.append(str(arr[num]))
+    arr.pop(num)
+
+print("<", ", ".join(ans), ">", sep="")
