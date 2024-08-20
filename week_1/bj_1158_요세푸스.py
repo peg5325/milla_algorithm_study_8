df1ec2bf108a546ae02a28a59e4ff62718fa443d
Eongyu Park <peg5325@gmail.com>
2024-08-20 23:52:13 +0900

    정답과 기존 답안 분리

--- week_1/bj_1158_요세푸스.py
+++ week_1/bj_1158_요세푸스.py
@@ -1,36 +1,39 @@
-from sys import stdin
+# from sys import stdin
+#
+#
+# input = stdin.readline
+#
+# n, k = map(int, input().split())
+#
+# arr = [i for i in range(1, n+1)]
+# result_arr = []
+# count = 1
+# index = 0
+#
+# while len(arr) != 0:
+#     if len(arr) == 1:
+#         result_arr.append(arr[0])
+#         break
+#
+#     if count == 3:
+#         result_arr.append(arr[index])
+#         del arr[index]
+#         index -= 1
+#         count = 0
+#
+#     if index == len(arr) - 1:
+#         index = 0
+#         count += 1
+#     else:
+#         index += 1
+#         count += 1
+#
+# print("<", end="")
+# print(*result_arr, sep=', ', end="")
+# print(">")
 
 
-input = stdin.readline
-
-n, k = map(int, input().split())
-
-arr = [i for i in range(1, n+1)]
-result_arr = []
-count = 1
-index = 0
-
-while len(arr) != 0:
-    if len(arr) == 1:
-        result_arr.append(arr[0])
-        break
-
-    if count == 3:
-        result_arr.append(arr[index])
-        del arr[index]
-        index -= 1
-        count = 0
-
-    if index == len(arr) - 1:
-        index = 0
-        count += 1
-    else:
-        index += 1
-        count += 1
-
-print("<", end="")
-print(*result_arr, sep=', ', end="")
-print(">")
+# --- 정답 ---
 
 n, k = map(int, input().split())
 ans = []
